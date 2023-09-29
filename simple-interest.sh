00985a5275237473b6e186a193582c8674de5a84
LOKESH M <115631279+loky0612@users.noreply.github.com>
2023-09-29 22:07:11 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # loky0612
    # Input:
    # p, principal amount
    # t, time period in years
